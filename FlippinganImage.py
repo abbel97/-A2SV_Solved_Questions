8a57b5185370f76a97b9e7f611450bcd7be6369a
abell1997 <abeltamerat1997@gmail.com>
2026-02-21 12:24:53 +0100

    Added file

---
+++ FlippinganImage.py
@@ -0,0 +1,6 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for i in range(len(image)):
+            image[i] = [1 - cell for cell in image[i][::-1]]
+        return image
+        
