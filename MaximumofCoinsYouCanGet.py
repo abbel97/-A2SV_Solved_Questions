2606a5a7e05fd6db8c87fa61920369d1c3fabc65
abell1997 <abeltamerat1997@gmail.com>
2026-02-25 19:11:57 +0100

    Added file

---
+++ MaximumofCoinsYouCanGet.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        n = len(piles)
+        coins = 0
+        
+        for i in range(n // 3, n, 2):
+            coins += piles[i]
+        return coins
