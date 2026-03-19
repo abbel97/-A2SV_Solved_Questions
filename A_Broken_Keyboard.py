3ffbf26d1e4970dbab3534264aca8cdc353359a9
abell1997 <abeltamerat1997@gmail.com>
2026-03-19 17:58:22 +0100

    Added file

--- A_Broken_Keyboard.py
+++ A_Broken_Keyboard.py
@@ -1,11 +1,20 @@
 t = int(input())
+
 for _ in range(t):
-    s = input()
-    char_count = {}
-    for char in s:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-    working_buttns = [char for char, count in char_count.items() if count % 2 == 1]
-    print(''.join(sorted(working_buttns)))
+    s = input().strip()
+    res = set()
+
+    i = 0
+    n = len(s)
+
+    while i < n:
+        j = i
+        while j < n and s[j] == s[i]:
+            j += 1
+        length = j - i
+        
+        if length % 2 == 1:
+            res.add(s[i])
+        i = j
+
+    print(''.join(sorted(res)))
